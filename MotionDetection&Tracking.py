7e44e884bba05c47278ab5dd437042953196dfcb
Arijit Dasgupta <arijit.rivalslayer@gmail.com>
2012-04-19 23:17:15 +0530

    Object tracking not working! Signed-off-by: Arijit Dasgupta <arijit.rivalslayer@gmail.com>

--- MotionDetection&Tracking.py
+++ MotionDetection&Tracking.py
@@ -80,7 +80,8 @@ fading_factor = 40 #Fading factor for sum image
 threshold_limit2_lower = 100 #Threshold for sum image calculation
 threshold_limit2_upper = 255
 skip = 10 #Good Feature Skipper
-param1 = 2 #Rotation parameter
+param1 = 1 #Rotation parameter
+motion_detection_skip_limit = 4
 
 #Primary initialization
 cv.CvtColor(img, gray_image, cv.CV_RGB2GRAY)
@@ -92,77 +93,84 @@ counter = 0
 
 #misc initialization
 flag = False #GoodFeatureToTrack execution flag
+motion_detection_skipper = 0
+
+#Defining the rotation check function (for debugging purposes)
+def rotation_check(x):
+    if x == 'C':
+        print "Did rotate"
+    elif x == 'F':
+        print "Didnt rotate"
 
 while True: #Main loop
-    #Acquiring the image
+    #Acquiring the image and grayscaling it
     img = cv.QueryFrame(capture)
+    cv.CvtColor(img, gray_image, cv.CV_RGB2GRAY)
     #Showing image
     if flag_true_image:
-        cv.ShowImage(window1, gray_image)
+        cv.ShowImage(window1, accumulator)
     else:
         cv.ShowImage(window1, render_image)
     #Image processing
-    cv.CvtColor(img, gray_image, cv.CV_RGB2GRAY)
-    cv.Copy(gray_image, register1_image)
-    cv.Smooth(register1_image, register1_image, cv.CV_GAUSSIAN, 3, 3)
-    cv.AbsDiff(register1_image, register2_image, accumulator)
-    cv.InRangeS(accumulator, (threshold_limit1_lower), (threshold_limit1_upper), accumulator)
-    cv.Dilate(accumulator, accumulator, None, 2)
-    cv.Add(accumulator, sum_image, sum_image, accumulator)
-    cv.SubS(sum_image, (fading_factor), sum_image)
-    cv.InRangeS(sum_image, (threshold_limit2_lower), (threshold_limit2_upper), accumulator)
-    cv.Copy(register1_image, register2_image)
-    #Motion detection
-    new_corners, status, track_error = cv.CalcOpticalFlowPyrLK(prev_image, gray_image, pyramid1, pyramid2, corners, (10,10), 2, (cv.CV_TERMCRIT_ITER, 10, 0), 0)
-    counter = (counter + 1) % skip
-    if(counter == 0):
-        corners = cv.GoodFeaturesToTrack(gray_image, eigen_image, temp_image, cornerCount = corner_count, qualityLevel = quality, minDistance = min_distance) #Good features to track
-        flag = True
-    cv.Copy(img, render_image)
-    cv.Copy(img, render_image)
-    cv.Copy(gray_image, prev_image)
-    #Drawing vectors and averaging the rotation...
-    sum = 0
-    summing_counter = 0
-    if flag:
-        flag = not flag
+    if (motion_detection_skipper > 0):
+        motion_detection_skipper = motion_detection_skipper - 1
     else:
-        for i in range(len(new_corners)):
-            try: #Have to fix the exception, for now, it's running by ignoring the index error...
-                x1, y1 = corners[i]
-                x1, y1 = int(x1), int(y1)
-                x2, y2 = new_corners[i]
-                x2, y2 = int(x2), int(y2)
-                avg = 0
-                if (cv.Get2D(accumulator, y1, x1)[0] >= 240):
-                    cv.Line(render_image, (x1, y1), (x2, y2), (0,255,255))
-                    cv.Circle(render_image, (x2, y2), 1, (0,255,0))
-                    sum = sum + (x2 - x1)
-                    summing_counter = summing_counter + 1
-                    avg = sum/summing_counter
-            except IndexError:
-                pass
+        cv.Copy(gray_image, register1_image)
+        cv.Smooth(register1_image, register1_image, cv.CV_GAUSSIAN, 3, 3)
+        cv.AbsDiff(register1_image, register2_image, accumulator)
+        cv.InRangeS(accumulator, (threshold_limit1_lower), (threshold_limit1_upper), accumulator)
+        cv.Dilate(accumulator, accumulator, None, 2)
+        cv.Add(accumulator, sum_image, sum_image, accumulator)
+        cv.SubS(sum_image, (fading_factor), sum_image)
+        cv.InRangeS(sum_image, (threshold_limit2_lower), (threshold_limit2_upper), accumulator)
+        cv.Copy(register1_image, register2_image)
+        #Motion detection
+        new_corners, status, track_error = cv.CalcOpticalFlowPyrLK(prev_image, gray_image, pyramid1, pyramid2, corners, (10,10), 2, (cv.CV_TERMCRIT_ITER, 10, 0), 0)
+        counter = (counter + 1) % skip
+        if(counter == 0):
+            corners = cv.GoodFeaturesToTrack(gray_image, eigen_image, temp_image, cornerCount = corner_count, qualityLevel = quality, minDistance = min_distance) #Good features to track
+            flag = True
+        cv.Copy(img, render_image)
+        cv.Copy(img, render_image)
+        cv.Copy(gray_image, prev_image)
+        #Drawing vectors and averaging the rotation...
+        sum = 0
+        summing_counter = 0
+        if flag:
+            flag = not flag
+        else:
+            for i in range(len(new_corners)):
+                try: #Have to fix the exception, for now, it's running by ignoring the index error...
+                    x1, y1 = corners[i]
+                    x1, y1 = int(x1), int(y1)
+                    x2, y2 = new_corners[i]
+                    x2, y2 = int(x2), int(y2)
+                    avg = 0
+                    if (cv.Get2D(accumulator, y1, x1)[0] >= 240):
+                        cv.Line(render_image, (x1, y1), (x2, y2), (0,255,255))
+                        cv.Circle(render_image, (x2, y2), 1, (0,255,0))
+                        sum = sum + (x2 - x1)
+                        summing_counter = summing_counter + 1
+                        avg = sum/summing_counter
+                except IndexError:
+                    pass
     #printing movement
     if avg > param1:
         print "Movement right"
-    elif avg < -param1:
-        print "Movement left"
-    if avg > param1:
         serial_port.write('R')
         x = serial_port.read()
-        if x == 'C':
-            print 'Rotated right'
-            corners = cv.GoodFeaturesToTrack(gray_image, eigen_image, temp_image, cornerCount = corner_count, qualityLevel = quality, minDistance = min_distance) #Good features to track
-        elif x == 'F':
-            print 'Didnt rotate right'
-    if avg > -param1:
+        rotation_check(x)
+        avg = 0
+        motion_detection_skipper = motion_detection_skip_limit
+        cv.AbsDiff(accumulator, accumulator, accumulator)
+    elif avg < -param1:
+        print "Movement left"
         serial_port.write('L')
         x = serial_port.read()
-        if x == 'C':
-            print 'Rotated left'
-            corners = cv.GoodFeaturesToTrack(gray_image, eigen_image, temp_image, cornerCount = corner_count, qualityLevel = quality, minDistance = min_distance) #Good features to track
-        elif x == 'F':
-            print 'Didnt rotate left'
+        rotation_check(x)
+        motion_detection_skipper = motion_detection_skip_limit
+        avg = 0
+        cv.AbsDiff(accumulator, accumulator, accumulator)
     #Runtime keystroke controls with flags
     key = cv.WaitKey(1)
     if(key == key_quit_lower or key == key_quit_upper):
